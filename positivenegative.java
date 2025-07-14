855af5a4302a1a23c8bf4b34ca3a22d9fa6be718
Nikitha153 <achinanikitha@gmail.com>
2025-07-14 19:26:20 +0530

    Create positivenegative.java

---
+++ positivenegative.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a=2;
+        if(a>0){
+        System.out.println("positive");
+    }else if(a<0){
+        System.out.println("negative");
+    }else{
+        System.out.println("zero");
+    }
+}
+}
